d5d5ab53a5e912973b10a7391be4e65f62eb1a91
Makastin <makastin14@gmail.com>
2014-12-12 09:36:28 -0430

    agrgada class movie

--- agenda/model.py
+++ agenda/model.py
@@ -1,8 +1,25 @@
 
 from sqlalchemy.schema import Column
 import sqlalchemy.types
-    
+from sqlalchemy import Unicode, Date    
 from camelot.admin.entity_admin import EntityAdmin
 from camelot.core.orm import Entity
 import camelot.types
-    
+    #class movie agragada
+class Movie(Entity):
+
+    __tablename__ = 'movie'
+
+    title = Column(Unicode(60), nullable = False)
+    short_description = Column(Unicode(512))
+    release_date = Column(Date())
+    genre = Column(Unicode(15))
+
+    def __unicode__(self):
+        return self.title or 'Untitled movie'
+
+    class Admin(EntityAdmin):
+        verbose_name = 'Movie'
+        list_display = ['title', 'short_description', 'release_date', 'genre']
+
+
